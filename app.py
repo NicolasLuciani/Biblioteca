3342fb31bf442760a0509a74affab824bbadc58c
NicolasLuciani <nicolasluciani1603@gmail.com>
2025-08-26 09:54:26 -0300

    menu

---
+++ app.py
@@ -0,0 +1,48 @@
+import time # importei a biblioteca time
+import os # importei a biblioteca os
+from classes import * # importei todas as classe do arquivo classes
+from funcoes import cadastrar, emprestar, qual_listar, atualizar_livros, excluir  # importei todas as funções do arquivo funcoes
+
+
+
+while True:                   # criação do menu
+    os.system("cls")
+    print("Bem vindo ao menu!")
+    print(30*"=")
+    time.sleep(1)
+    print("Você está no sistema da biblioteca, selecione uma opção.")
+    time.sleep(1)
+
+    print("1- CADASTRO DE LIVRO \n2- EMPRESTAR LIVRO \n3- LISTAR \n4- ATUALIZAR \n5- EXCLUIR \n0- S")
+    time.sleep(1)
+
+    escolha = int(input("--> "))
+    time.sleep(1)
+    
+
+    if escolha == 1:        # opção de cadastro
+        cadastrar()
+
+    elif escolha == 2:
+        emprestar()
+        
+    elif escolha == 3:
+        qual_listar()
+        
+    elif escolha == 4:
+        atualizar_livros()
+
+    elif escolha == 5:
+        excluir()
+
+    elif escolha == 0:
+        print("SAINDO...")
+        time.sleep(1)
+        os.system("pause")
+
+        break
+
+    else:
+        print("ESCOLHA INVALIDA")
+        time.sleep(1)
+        os.system("pause")
